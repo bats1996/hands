cfd3a5af860e909cad98b355d06af7f234642cdb
bats1996 <bats1996@gmail.com>
2020-06-30 14:36:16 +0200

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Holding-Hands
+
+
+As we live in times where holding hands might spread a deadly virus, times where politicians are shaking elbows, times where touching your own face is dangerous! Here is the solution: do it online.
+
+Holding hands allows users the opportunity to hold hand respecting physical distancing, you can shake hands at the end of a meeting, or re-create the creation of Adam.
+
+
+
---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+// where everyone's data got sent to everyone else
+require("dotenv").config()
+const express = require("express");
+const http = require("http");
+const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+
+// https://expressjs.com/en/starter/static-files.html
+app.use(process.env.BASE, express.static(__dirname));
+app.get(process.env.BASE,(request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+var io = require("socket.io")(server);
+
+var serverData = {}; // everyone's data
+
+function newConnection(socket) {
+  console.log("new connection: " + socket.id);
+
+  socket.on("client-start", onClientStart);
+  socket.on("client-update", onClientUpdate);
+  socket.on("disconnect", onClientExit);
+
+  function onClientStart() {
+    // send an update every 10 milliseconds
+    setInterval(function() {
+      socket.emit("server-update", serverData);
+    }, 10);
+  }
+
+  // client is sending us an update
+  function onClientUpdate(data) {
+    serverData[socket.id] = data;
+  }
+
+  // bye bye
+  function onClientExit() {
+    delete serverData[socket.id];
+    console.log(socket.id + " disconnected");
+  }
+}
+
+console.log("listening...");
+io.sockets.on("connection", newConnection);
